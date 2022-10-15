5f16630cd183ece7fa84b48e2f6f56c9ea2fb58e
Tajjudeen Abduhhadee <tajjudeenabduhhadee@hotmail.co.uk>
2022-10-15 03:20:32 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ *Return: Always 0 (succeed)
+ **/
+int main(void)
+{
+	char letters;
+	char LETTERS;
+
+	letters = 'a';
+	LETTERS = 'A';
+	while
+		(letters <= 'z')
+		{
+		       putchar(letters);
+		       letters++;
+		}
+	while
+		(LETTERS <= 'z')
+		{
+			putchar(LETTERS);
+			LETTERS++;
+		}
+	return (0);
+}
