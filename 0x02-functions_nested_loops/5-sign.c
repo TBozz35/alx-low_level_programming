2add1e130daf8104b5db0671ea9ec2d2bbcc27aa
Tajjudeen Abduhhadee <tajjudeenabduhhadee@hotmail.co.uk>
2022-10-18 21:10:04 -0700

    Task 4, 5 & 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,19 +4,22 @@
  *
  * Return: Always 0
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if (c >= 'a' && c <= 'z')
+	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
-	else if (c >= 'A' && c <= 'Z')
+	else if (n < 0)
 	{
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
+		_putchar('0');
 		_putchar('\n');
 		return (0);
-	}							
+	}
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Check code
+ *
+ * Return: Always 0
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
